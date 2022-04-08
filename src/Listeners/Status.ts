7ee613319031fc3c953bde4d6f199935ec3e2d21
RediPanda <redi@nekonii.xyz>
2022-04-08 16:26:50 +1000

    Fix: Changed the bot's status message.

--- src/Listeners/Status.ts
+++ src/Listeners/Status.ts
@@ -14,16 +14,6 @@ export default class StatusListener extends Listener {
         const ListenLogger = new Logger("Events - Client", true);
         ListenLogger.log(LoggerType.DEBUG, `Event received <- ['Status': ReadyEvent]!`);
 
-        // Loop for every 5 minutes with an up to date member count.
-        if (client.user?.id === "919172638327312456") {
-            client.user?.setActivity("If you see this bot online, it usually means someone is developing something :)");
-        } else {
-            setInterval(async () => {
-                const YulxVol = await client.guilds.fetch('917479312825933884');
-                await YulxVol.fetch();
-                const count = YulxVol.approximateMemberCount;
-                client.user?.setActivity(`over ${count} users`, {type: 'WATCHING'});
-            }, 1000 * 60 * 5);
-        }
+        client.user?.setActivity(`over 5 tasks registered!`, {type: "WATCHING"});
     }
 }
