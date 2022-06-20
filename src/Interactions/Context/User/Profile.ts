3073162066c9e127b91f8eab1ec59a775996e089
RediPanda <redi@nekonii.xyz>
2022-06-21 00:14:31 +1000

    Mod: Changed the way we view tasks on profiles and added the metrics timer control command.

--- src/Interactions/Context/User/Profile.ts
+++ src/Interactions/Context/User/Profile.ts
@@ -1,46 +1,37 @@
-import { ApplicationCommandOptionData, ApplicationCommandType, ColorResolvable, CommandInteraction, Guild, GuildMember, MessageEmbed, Webhook } from "discord.js";
+import { ApplicationCommandOptionData, ApplicationCommandType, ColorResolvable, CommandInteraction, ContextMenuInteraction, Guild, GuildMember, Interaction, MessageEmbed, Webhook } from "discord.js";
 import InteractionEvent from "./Base";
-import { UserProfile } from "../../Framework/Factory/UserProfile";
+import { UserProfile } from "../../../Framework/Factory/UserProfile";
 import type { AkairoClient } from "discord-akairo";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import RelativeTime from "dayjs/plugin/relativeTime";
-import { TaskManager } from "../../Framework/Factory/Task";
-
-dayjs.extend(duration);
-dayjs.extend(RelativeTime);
+import { TaskManager } from "../../../Framework/Factory/Task";
 
 export interface InteractionRegister {
     description?: string,
     name: string,
     type: ApplicationCommandType
-    options?: Array<ApplicationCommandOptionData>
 }
 
 /**
  * @class Interaction Event.
- * @description This is the base Class. New Interactions should extend 
+ * @description This is the base Class. New Interactions should extend upon this class.
  */
-export default class Profile extends InteractionEvent {
+export default class RemoteProfile extends InteractionEvent {
     constructor() {
-        // NOOP. (Empty for most cases, but here incase you need persisass.)
-        super();
+       super();
     }
 
     /**
      * @method Register
-     * @description A method of the Interaction class for registering tta.
+     * @description A method of the Interaction class for registering the interaction with metadata.
      * @returns <InteractionRegister> (Enum).
      */
     override register(): InteractionRegister {
-        // You may log any interactions here that needs to be instantiated before the execution starts.
-        // You can think of this as the constructor of the class.
-        // new Logger("Interaction Register - Base", true).log(LoggerType.ERROR, `${LC.FgRed}${LC.BgWhite}Instance has not been overidden.`)
-
         return {
-            description: "Fetches your profile for the week.",
-            name: "profile",
-            type: "CHAT_INPUT"
+            // description: "This is a base interaction.",
+            name: "Profile",
+            type: "USER"
         }
     }
 
@@ -51,8 +42,17 @@ export default class Profile extends InteractionEvent {
      * @returns boolean.
      */
     override async interactionPerm(member: GuildMember): Promise<boolean> {
-        // This supports promises!
-        // console.log(member.id);
+        return await true;
+    }
+
+    /**
+     * @method handlePerm
+     * @description A method for determining whether there should be pre-flight checks before sending to the handler.
+     * @param interaction Interaction of the handler.
+     * @param member Guild Member of the interactor.
+     * @returns boolean
+     */
+    override async handlePerm(interaction: Interaction, member: GuildMember): Promise<boolean> {
         return await true;
     }
 
@@ -63,13 +63,13 @@ export default class Profile extends InteractionEvent {
      * @returns void
      */
     // You will need to cast the Interaction type to your needs since the listener handler already manages this. (For Intellisense)
-    override async execute(interaction: CommandInteraction): Promise<void> {
-        let UP = new UserProfile(interaction.client, interaction.user.id)
+    override async execute(interaction: ContextMenuInteraction): Promise<void> {
+        let UP = new UserProfile(interaction.client, interaction.targetId)
         let TM = new TaskManager(interaction.client);
 
-        let weektaskdata = TM.GetTasksInTwoWeek(interaction.user.id);
+        let weektaskdata = TM.GetTasksInTwoWeek(interaction.targetId);
         let weektaskid = weektaskdata.map(wt => wt.getId());
-        let alltasks = TM.GetTasks(interaction.user.id);
+        let alltasks = TM.GetTasks(interaction.targetId);
         let overalldata = [];
         
         for (let i = 0; i < alltasks.length; i++) {
@@ -87,7 +87,7 @@ export default class Profile extends InteractionEvent {
                 thisweekcontent += `\`${weektaskdata[i].getActiveState()}\` **[${weektaskdata[i].getClassId()}]** - [${weektaskdata[i].getName()}](${weektaskdata[i].getRubricLink() || "https://google.com.au"} "${weektaskdata[i].getId()}") - (<t:${dayjs(weektaskdata[i].getDueDate()).unix()}:R>) [(Submit?)](${weektaskdata[i].getSubmitLink()} "Submission link")\n`
             }
         } else {
-            thisweekcontent = `You have no tasks due in 2 weeks. You are up to date! ${(interaction.client as AkairoClient).getEmoji('kleeexcited')}`
+            thisweekcontent = `They have no tasks due in 2 weeks; they are up to date! ${(interaction.client as AkairoClient).getEmoji('kleeexcited')}`
         }
 
         // Append total content:
@@ -102,25 +102,31 @@ export default class Profile extends InteractionEvent {
                 overallcontent += `\`${overalldata[i].getActiveState()}\` **[${overalldata[i].getClassId()}]** - [${overalldata[i].getName()}](${overalldata[i].getRubricLink() || "https://google.com.au"} "${overalldata[i].getId()}") - (<t:${dayjs(overalldata[i].getDueDate()).unix()}:R>) [(Submit?)](${overalldata[i].getSubmitLink()} "Submission link")\n`
             }
         } else {
-            overallcontent = `You have no tasks registered. You are up to date! ${(interaction.client as AkairoClient).getEmoji('kleeexcited')}`
+            overallcontent = `They have no tasks registered and are up to date! ${(interaction.client as AkairoClient).getEmoji('kleeexcited')}`
         }
 
         let embed = new MessageEmbed()
             .setColor(UP.GetColor() as ColorResolvable)
-            .setTitle(`${(interaction.member as GuildMember).displayName}'s Profile`)
+            .setTitle(`Remote Profile`)
             .setThumbnail((await UP.AbstractWebhook()).avatarURL() as string)
-            .setDescription(`${(interaction.client as AkairoClient).utils.getToastMessage()}, **${(interaction.member as GuildMember).displayName}**!
-
-You have accumulated over \`${dayjs.duration({seconds: UP.GetStatData()?.cumulative_study as number || 0}).humanize()}\` worth of **studying** over the bot's tracking history whilst balancing it with \`${dayjs.duration({seconds: UP.GetStatData()?.cumulative_break as number || 0}).humanize()}\` worth of **break**.        
+            .setDescription(`
+They have accumulated over \`${dayjs.duration({seconds: UP.GetStatData()?.cumulative_study as number || 0}).humanize()}\` worth of **studying** over the bot's tracking history whilst balancing it with \`${dayjs.duration({seconds: UP.GetStatData()?.cumulative_break as number || 0}).humanize()}\` worth of **break**.        
+            
 `)
 
             .addField(`Tasks due in the new few weeks ―――― [${weektaskdata.length}]`, thisweekcontent)
-            .addField(`Overall tasks ―――――――――――― [${TM.GetTasks(interaction.user.id).length}]`, overallcontent)
+            .addField(`Overall tasks ―――――――――――― [${overalldata.length}]`, overallcontent)
 
         interaction.reply({embeds: [embed]})
     }
 
-    handlePerm(): boolean {
-        return true;
+    /**
+     * @method handleInteraction
+     * @description The function that handles side interactions (Buttons and Select Menus.)
+     * @param interaction Interaction of the event emitted.
+     * @returns void
+     */
+    override handleInteraction(interaction: Interaction): void {
+        throw new Error("PROTOCOL NOT OVERRIDDEN");
     }
 }
---
+++ src/Interactions/Slash/Stop.ts
@@ -0,0 +1,100 @@
+import { ApplicationCommandOptionData, ApplicationCommandType, ColorResolvable, CommandInteraction, Guild, GuildMember, MessageEmbed, Webhook } from "discord.js";
+import InteractionEvent from "./Base";
+import { MetricType, Status, UserProfile } from "../../Framework/Factory/UserProfile";
+import type { AkairoClient } from "discord-akairo";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import RelativeTime from "dayjs/plugin/relativeTime";
+import { TaskManager } from "../../Framework/Factory/Task";
+
+dayjs.extend(duration);
+dayjs.extend(RelativeTime);
+
+export interface InteractionRegister {
+    description?: string,
+    name: string,
+    type: ApplicationCommandType
+    choices?: Array<any>
+    options?: Array<ApplicationCommandOptionData>
+}
+
+/**
+ * @class Interaction Event.
+ * @description This is the base Class. New Interactions should extend 
+ */
+export default class StopCMD extends InteractionEvent {
+    constructor() {
+        // NOOP. (Empty for most cases, but here incase you need persisass.)
+        super();
+    }
+
+    /**
+     * @method Register
+     * @description A method of the Interaction class for registering tta.
+     * @returns <InteractionRegister> (Enum).
+     */
+    override register(): any { // The any is a cheat here as we need to directly change the data.
+        // You may log any interactions here that needs to be instantiated before the execution starts.
+        // You can think of this as the constructor of the class.
+        // new Logger("Interaction Register - Base", true).log(LoggerType.ERROR, `${LC.FgRed}${LC.BgWhite}Instance has not been overidden.`)
+
+        return {
+            description: "Stops the metric timer.",
+            name: "stop",
+            type: "CHAT_INPUT",
+            options: [{
+                type: 3,
+                name: "metric",
+                description: "Choose the type of metrics you wish to stop the timer for.",
+                choices: [
+                    {name: "Study", value: "study"},
+                    {name: "Break", value: "break"}
+                ]
+            }]
+        }
+    }
+
+    /**
+     * @method interactionPerm
+     * @description A method for determining whether the interaction should be handled or not.
+     * @param member Guild Member instance.
+     * @returns boolean.
+     */
+    override async interactionPerm(member: GuildMember): Promise<boolean> {
+        // This supports promises!
+        // console.log(member.id);
+        return await true;
+    }
+
+    /**
+     * @method Execute
+     * @description The main driver of the class.
+     * @param interaction Interaction of the event emitted.
+     * @returns void
+     */
+    // You will need to cast the Interaction type to your needs since the listener handler already manages this. (For Intellisense)
+    override async execute(interaction: CommandInteraction): Promise<void> {
+        let metric = interaction.options.getString('metric');
+        let EMBED = new MessageEmbed();
+
+        // Cancel the interaction if no parameter was provided.
+        if (metric === null) {
+            EMBED.setDescription("No metric parameter has been provided!")
+            EMBED.setColor("RED")
+            interaction.reply({embeds: [EMBED], ephemeral: true})
+            return;
+        }
+
+        let UP = new UserProfile(interaction.client, interaction.user.id);
+        UP.MeasureStatistics(Status.AVAILABLE, metric as MetricType);
+
+        // Create embed.
+        EMBED.setColor("GREEN")
+        EMBED.setDescription(`You have successfully stopped the timer for \`${metric}\``);
+        interaction.reply({embeds: [EMBED]});
+    }
+
+    handlePerm(): boolean {
+        return true;
+    }
+}
