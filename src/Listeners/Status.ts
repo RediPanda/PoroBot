96de06b8ae96102cc3d9fdab2fa07c3fe9a8d2fa
RediPanda <redi@nekonii.xyz>
2022-06-21 15:12:26 +1000

    Fix: Set the dynamic counter to the true size instead of a static placeholder.

--- src/Listeners/Status.ts
+++ src/Listeners/Status.ts
@@ -1,6 +1,7 @@
 import { Listener } from "discord-akairo";
 import type { Client } from "discord.js";
 import { Logger, LoggerType } from "../Framework/IO/Logger";
+import { Storage, StorageType } from "../Framework/IO/Storage";
 
 export default class StatusListener extends Listener {
     constructor() {
@@ -11,9 +12,10 @@ export default class StatusListener extends Listener {
     }
 
     override async exec(client: Client): Promise<void> {
+        const storage = new Storage({server: '0', user: '0'}).select(StorageType.Task).stream();
         const ListenLogger = new Logger("Events - Client", true);
         ListenLogger.log(LoggerType.DEBUG, `Event received <- ['Status': ReadyEvent]!`);
 
-        client.user?.setActivity(`over 5 tasks registered!`, {type: "WATCHING"});
+        client.user?.setActivity(`over ${storage.array().length} tasks registered!`, {type: "WATCHING"});
     }
 }
