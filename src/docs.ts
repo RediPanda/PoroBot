4bc3bdb1d0d1f9c5b2372af8b4fbcf0860e99dfe
RediPanda <redi@nekonii.xyz>
2022-08-13 11:48:20 +1000

    Doc: Updated doc naming and more exports

--- src/docs.ts
+++ src/docs.ts
@@ -5,12 +5,19 @@ export { Core } from './index'
 export { GuildManager } from './Framework/Factory/Guild';
 export { MemberManager } from './Framework/Factory/GuildMember';
 //export { WebhookManager } from './Framework/Factory/Webhook';
+export { Task, TaskManager, TaskModel, ActiveState, ActiveStateColor, ActiveStateOptionData } from "./Framework/Factory/Task"
+// export { AnalyticEngine } from "./Framework/Factory/Analytics"
+export { SettingsManager } from "./Framework/Factory/SettingsManager"
+export { UserProfile, UserProfileModel, WeekModel, StatModel, Status, TimeModel, MetricType } from "./Framework/Factory/UserProfile"
+export { Utility } from "./Framework/Factory/Utility"
 
 // Framework/Graphics
-export { Drawable } from "./Framework/Graphics/Canvas";
+export { Drawable, Position, TrianglePosition, ShapeOptions, Size, StrokeOptions } from "./Framework/Graphics/Canvas";
 
 // Framework/IO
 export { Logger, LoggerType, LC } from './Framework/IO/Logger';
+export { Query } from "./Framework/IO/Query";
+export { Storage, StorageParam, StorageType } from "./Framework/IO/Storage";
 
 // Interactions
-export { default } from "./Interactions/Context/User/Base";
+export { default, InteractionRegister } from "./Interactions/Context/User/Base";
